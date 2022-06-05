610bc2fe548730153f51a664e304550a585994b9
MD <43723938+MaelMD@users.noreply.github.com>
2022-06-05 21:16:48 +0100

    Update caoexo1b.m

--- MATLAB/caoexo1b.m
+++ MATLAB/caoexo1b.m
@@ -1,35 +1,29 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%
 %%   Solution Matlab de l'exercice 1 du projet CAO
-%%   Construction d'une courbe de Bézier 
-%%   avec tracé du polygone de contrôle
+%%   Construction d'une courbe de BÃ©zier 
+%%   avec tracÃ© du polygone de contrÃ´le
 %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%% Introduction au calcul scientifique par la pratique %%%%%%%
-%%%%%%%    I. Danaila, P. Joly, S. M. Kaber et M. Postel    %%%%%%%
-%%%%%%%                 Dunod, 2005                         %%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%
-%%   Exemple d'un polygone de contrôle convexe
+%%   Exemple d'un polygone de contrÃ´le convexe
 %%
 clear all; close all;
-% définition des points de contrôle 
+% dÃ©finition des points de contrÃ´le 
 np=5;
 XP=zeros(np,1);YP=zeros(np,1);
 XP=[ 0. , 1. , 2. , 3. , 3.5   ] ;
 YP=[ 0. , 2.5 , 3. , 1.5 , 0.   ] ;
-% échantillonage de la courbe de Bézier
+% Ã©chantillonage de la courbe de BÃ©zier
 T=[0:0.05:1.];
 [X,Y]=cbezier(T,XP,YP);
 %
-% représentation graphique  
+% reprÃ©sentation graphique  
 %
-% a) définition de la figure 
+% a) dÃ©finition de la figure 
 nf=1; figure(nf) ; hold on ;
 xmin=min(XP)-0.5;xmax=max(XP)+0.5;
 ymin=min(YP)-0.5;ymax=max(YP)+0.5;
 axis([xmin,xmax,ymin,ymax]);
-% b) tracé de la courbe de Bézier
+% b) tracÃ© de la courbe de BÃ©zier
 color='r';pchar='P';pcolor='b';ptrait='--';
 pbezier(X,Y,XP,YP,color,pchar,pcolor,ptrait)
-title('Une courbe de Bézier avec son polygone de contrôle'); hold off ;
+title('Une courbe de BÃ©zier avec son polygone de contrÃ´le'); hold off ;
